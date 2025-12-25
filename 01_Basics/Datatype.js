dff8d6472b6fb84bb747bb18f6e92a4e31721a79
Meet-Dadhaniya <patelmeet2669@gmail.com>
2025-12-25 18:51:12 +0530

    Learned about Datatype in JS

---
+++ 01_Basics/Datatype.js
@@ -0,0 +1,32 @@
+"use strict"; /* if write it at top it
+                 treat all js code as new version*/
+
+//alert(3+3)  /* it is used to give alert message and pop
+//              message but the pop shows in browser but
+//              for now we are using node js*/         
+
+let name="Meet" //aumatically identify that it's datatype is string.
+let age=19 //aumatically identify that it's datatype is int.
+let isLoggedin = true //Identify that it is boolean
+
+
+//--Data types-- (Premitive Data type)
+
+// int/number
+// Bigint used to store large number
+// boolean means true or false
+// null --> is a standalone value
+// undefined --> representation of empty value
+// symbol --> mostly used in react for uniqueness
+
+
+// --other datatypes---
+
+// object
+
+console.log(typeof age); //type of is used to check datatype of variable.
+
+
+console.log(typeof null);//--Imp null's data type is object
+
+console.log(typeof undefined); //undifined itself is a datatype
