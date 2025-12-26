9d4794303927344e53fd32a3551516dda499c3a2
Meet-Dadhaniya <patelmeet2669@gmail.com>
2025-12-26 20:56:46 +0530

    Learned about datatype conversion and operation

---
+++ 01_Basics/Datatype_conversion.js
@@ -0,0 +1,41 @@
+
+let score=true
+
+console.log(typeof score);
+
+let valueInnumber = Number(score); //always write first letter of datatype in capita like here Number, String etc
+
+console.log(typeof valueInnumber );
+console.log(valueInnumber); //NaN
+
+
+//"33"(any number)written in string can convert in number datatype.
+//"33abc"(number and letter combation in string) it will not convert in number datatype it will give Null as output.
+// true convert in 1 and false convert in 0 or visa versa
+
+let isLoggedin = 1
+
+let valueinLoggedin = Boolean(isLoggedin)
+
+console.log(typeof valueinLoggedin); // output = boolean
+
+let emptyval = "Meet"
+
+let valueInempty  = Boolean(emptyval)
+
+console.log( valueInempty);
+ 
+// 1 convert in true and 0 convert to false
+// "" (empty string) when we try to convert empty string into boolean it will give false as output
+// "Meet" (filled string) when we try to convert filled string into boolean it will give true as output
+
+let num=11
+
+let strinconvert = String(num)
+
+console.log(strinconvert);
+console.log(typeof strinconvert);
+
+//as per the upper ex we can conert number to string 
+
+
