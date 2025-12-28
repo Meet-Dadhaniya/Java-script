878da3a28426219469273db53c5f89aa2cae62de
Meet-Dadhaniya <patelmeet2669@gmail.com>
2025-12-28 16:22:49 +0530

    Learned about string and it's method

---
+++ 01_Basics/string.js
@@ -0,0 +1,36 @@
+const name = "Meet"
+const surname = "Patel"
+
+console.log(name + surname); // Meet Patel
+
+const name1 = "Meet"
+const num = 50
+
+console.log(name1 + num); //Meet50
+
+const myname = "Meet"
+
+console.log(`My name is ${myname}`);
+
+console.log(myname[1]);
+
+//some method of string
+
+console.log(myname.length);
+console.log(myname.toUpperCase());
+console.log(myname.charAt(2));
+console.log(myname.indexOf("t"));
+
+console.log(myname.substring(1,3)); // output --> ee
+
+console.log(myname.slice(0,3));
+console.log(myname.slice(-4,3));
+console.log(myname.includes('e'));
+
+const n= "Meet-Patel"
+
+console.log(n.split("-"));
+
+
+
+
