94b7143d6be859f62d7be461357672368433b43c
Meet-Dadhaniya <patelmeet2669@gmail.com>
2026-01-04 15:59:17 +0530

    Learned about basics of array and method in js

---
+++ Array/array1.js
@@ -0,0 +1,36 @@
+
+//declaration of array
+const myArr = [1,2,3,4,5]
+
+// console.log(myArr[2]); //accessing  element of array
+
+// // Array method
+
+// myArr.push(7)
+// console.log(myArr);
+// myArr.pop() // no need to give argument it will always remove last element from array
+// console.log(myArr);
+
+// myArr.unshift(4)
+// console.log(myArr);
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(10))
+// const nArr= myArr.join()
+
+// console.log(nArr);
+// console.log(typeof nArr); // string
+
+const n1=myArr.slice(1,4)
+console.log(n1);
+console.log(myArr);
+
+const n2 = myArr.splice(1,3)
+console.log(n2);
+console.log(myArr);
+
+
+
+
+
