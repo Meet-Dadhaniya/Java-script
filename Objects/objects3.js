744809f42e42027c069ef67533853021d3cac441
Meet-Dadhaniya <patelmeet2669@gmail.com>
2026-02-22 17:17:59 +0530

    learned about object de-structure

---
+++ Objects/objects3.js
@@ -0,0 +1,12 @@
+  
+  // --- we will see obj de-structure ----
+
+const course = {
+    coursename:"js in hindi",
+    price : "999",
+    instructor : "meet"
+}
+
+const {instructor} = course
+
+console.log(instructor);
